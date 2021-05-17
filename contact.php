6a6e9a532a330e95e548622e7e768aeac5957210
Steven Boeren <70892306+stevenboeren@users.noreply.github.com>
2021-05-17 15:23:38 +0200

    Contact-Form Nieuwe Styling!!

--- contact.php
+++ contact.php
@@ -10,26 +10,25 @@
 		?>
 		<main>
 			<h1>Contact</h1> <br>
-			<h2>Send e-mail to dumbledoretavern@gmail.com</h2>
+			<h2>Send e-mail to dumbledoretavern@gmail.com</h2><br>
 			<form class="contact-form" action="backend/contactController.php" method="post">
 				<input type="text" name="name" placeholder="Full name"><br>
 				<input type="text" name="mail" placeholder="Your e-mail"><br>
 				<input type="text" name="subject" placeholder="Subject"> <br>
 				<textarea name="message" placeholder="Message"></textarea><br>
-				<button type="submit" name="submit">Send</button><br>
-			</form>
+				<button class="submit" type="submit" name="submit">Send</button><br>
+			</form><br>
+			<h2>Visiting hours restaurant (without Covid-19):</h2><br>
 			<h4>
-			Email: dumbledoretavern@gmail.com</h4>
-			<br>
-			<h4>(without Covid-19) <br>
-			Visiting hours restaurant:<br>
 			Monday until Friday: <br>
 			Open: 12:00 <br>
 			Closed: 20:00 <br>
 			Weekend: <br>
 			Open: 13:00 <br>
 			Closed: 00:00 <br>
-			Visiting hours maze: <br>
+			</h4><br>
+			<h2>Visiting hours maze (without Covid-19):</h2>
+			<br><h4>
 			Monday until Thursday: <br>
 			Open: 12:00 <br>
 			Closed: 20:00 <br>
